ab2811ea48e1845dbcef3e2cb939276d5571fd5a
victor-at-wiris <victor.santiago@wiris.com>
2022-12-14 08:38:36 +0100

    chore

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 // Head customization with the shadow
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+let button = document.getElementById('uploadExcel').onclick = function () {
+
+    this.parseExcel = function (file) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            var data = e.target.result;
+            var workbook = XLSX.read(data, {
+                type: 'binary'
+            });
+
+            workbook.SheetNames.forEach(function (sheetName) {
+                // Here is your object
+                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                var json_object = JSON.stringify(XL_row_object);
+                console.log(json_object);
+
+            })
+
+        };
+
+        reader.onerror = function (ex) {
+            console.log(ex);
+        };
+
+        reader.readAsBinaryString(file);
+    };
+};
