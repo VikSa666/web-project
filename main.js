1ff67bacb3987dc38e9bc4d5de2b13ca7fd1eb32
victor-at-wiris <victor.santiago@wiris.com>
2022-12-14 08:55:15 +0100

    chore not working

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// Head customization with the shadow
-window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-})
-
 document.getElementById('uploadExcel').onchange = function () {
 
     this.parseExcel = function (file) {
